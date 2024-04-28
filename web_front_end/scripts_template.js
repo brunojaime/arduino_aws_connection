203ee181f7f401b5bc7d63adb907eec08e4d3115
Bruno <bruno.jaime@live.com.ar>
2024-04-28 19:20:08 -0300

    Creating front end

---
+++ web_front_end/scripts_template.js
@@ -0,0 +1,45 @@
+import config from './end_poinst.js'; //File with the endpoints
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    // Crea un objeto para almacenar los datos del formulario.
+    const formData = new FormData(form);
+    const formProps = {};
+    formData.forEach((value, key) => {
+        formProps[key] = value;
+    });
+
+    // Convertir los datos del formulario a JSON.
+    const jsonData = JSON.stringify(formProps);
+
+    try {
+        const response = await fetch('https://your_api', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonData,
+        });
+        alert('Datos cargados correctamente'); // Asumir éxito si no hay excepción
+    } catch (error) {
+        console.error('Fetch error: ', error);
+        alert('Hubo un problema en la carga');
+    }
+    
+});
+
+const dateTimeInput = document.getElementById('date_time');
+dateTimeInput.valueAsNumber = Date.now();
+
+const categoriesSelect = document.getElementById('categories');
+const categories = ['cat1', 'cat2', 'cat3'];
+
+categories.forEach((category) => {
+    const option = document.createElement('option');
+    option.value = category;
+    option.textContent = category;
+    categoriesSelect.appendChild(option);
+});
